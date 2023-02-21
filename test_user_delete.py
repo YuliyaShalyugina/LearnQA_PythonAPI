f6990dfa5ee998c35f57cd1740f887e21b319c54
dzhulia08 <123802723+dzhulia08@users.noreply.github.com>
2023-02-21 18:45:46 +0300

    Ex18: add test_user_delete.py

---
+++ test_user_delete.py
@@ -0,0 +1,105 @@
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserDelete(BaseCase):
+    #Попытка удалить пользователя по ID 2
+    def test_delete_user_as_same_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id = self.get_json_value(response1, "user_id")
+
+        response2 = MyRequests.delete(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response2, 400)
+        assert response2.content.decode("utf-8") == "Please, do not delete test users with ID 1, 2, 3, 4 or 5.", \
+            f"Unexpected response content {response2.content}"
+
+
+    # Второй - позитивный. Создать пользователя, авторизоваться из-под него, удалить,
+    # затем попробовать получить его данные по ID и убедиться, что пользователь действительно удален.
+    def test_positive_delete_just_created_user_with_authorization_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # DELETE OTHER USER
+        response3 = MyRequests.delete(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response4, 404)
+        assert response4.content.decode("utf-8") == "User not found", \
+            f"Unexpected response content {response4.content}"
+
+
+    # Третий - негативный, попробовать удалить пользователя, будучи авторизованными другим пользователем.
+    def test_negative_delete_just_created_user_with_authorization_other_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # DELETE OTHER USER
+        response3 = MyRequests.delete(
+            f"/user/{user_id}2",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response3, 422)
